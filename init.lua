e1cb9036ecc55918afc375abd3aa2e3cf53c44e1
ammar-faifi <me@ammarf.sa>
2024-06-08 10:16:49 +0300

    customize `netrw`

--- init.lua
+++ init.lua
@@ -1,6 +1,12 @@
 -- [[ Options ]]
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+-- Netrw settings
+vim.g.netrw_hide = 1
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_browse_split = 3
+
 vim.g.have_nerd_font = true
 vim.opt.number = true
 vim.opt.relativenumber = true
