17fc8b87c56b2c6798bb25a74fb6fca9f29d5662
ammar-faifi <me@ammarf.sa>
2025-01-30 21:04:08 +0300

    unify name, clean up structure

--- init.lua
+++ init.lua
@@ -406,6 +406,14 @@ require('lazy').setup {
       require('nvim-treesitter.configs').setup(opts)
     end,
   },
+
+  { -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = 'ibl',
+    opts = {},
+  },
 }
 
 -- vim: ts=2 sts=2 sw=2 et
